d6fd4be3d553eb00e5391a4b84726cda3503b81b
jpfalcuci <jpfalcuci@gmail.com>
2024-03-24 16:38:56 -0300

    feat: Aula 1 - C

---
+++ c/aula_01/insertValores.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+  int num = 0;
+  printf("Informe o valor do número: ");
+  scanf("%d", &num);
+  
+  printf("O número digitado é: %d", num);
+  
+  return 0;
+}
